c280e8163c850d028612972cc0ca464c71c1d39b
yuewan2 <wanyue1996@gmail.com>
2019-11-27 21:10:06 -0500

    helper function

---
+++ utils/func.py
@@ -0,0 +1,14 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
