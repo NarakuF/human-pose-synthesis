37ea88aba5405cffc4885a6edbe0195c0240004a
yuewan2 <wanyue1996@gmail.com>
2019-11-28 01:31:09 -0500

    Update

--- model/discriminator.py
+++ model/discriminator.py
@@ -17,32 +17,32 @@ class Discriminator(nn.Module):
     def __init__(self, embeddings):
         super(Discriminator, self).__init__()
         self.hidden_size = 64 # output encoded annotation size
-        self.image_size = 64 # input image size before going through CNN
+        self.image_size = 256 # input image size before going through CNN
         self.output_size = 64 # output image representation size after going through CNN
         self.emb_layer = create_emb_layer(embeddings, non_trainable=True)
         self.rnn = nn.GRU(input_size = embeddings.size()[1], 
                           hidden_size = self.hidden_size, 
                           num_layers = 2,
                           batch_first = True)
-        ndf = 64
+        ndf = 128
         self.main = nn.Sequential(
             # input is (nc=3) x 64 x 64
-            nn.Conv2d(3, ndf, 4, 2, 1, bias=False),
+            nn.Conv2d(3, ndf*2, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 32 x 32
-            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 2),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 16 x 16
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.Conv2d(ndf*2, ndf * 4, 8, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(ndf * 4, ndf * 8, 8, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(ndf * 8, ndf * 16, 8, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 16),
+            nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, self.output_size, 4, 1, 0, bias=False),
+            nn.Conv2d(ndf * 16, self.output_size, 4, 1, 0, bias=False),
             nn.BatchNorm2d(self.output_size)
         )
         self.fc = nn.Linear(self.output_size+self.hidden_size, 1)
@@ -56,7 +56,8 @@ class Discriminator(nn.Module):
         encoded_annotate = torch.reshape(x[:,-1,:], (-1, x[:,-1,:].shape[1], 1, 1))
         encoded_img = self.main(image)
         
-        # *最后一层fully connected layer的input (concatenate encoded_annotate and encoded_img)   
+        # *最后一层fully connected layer的input (concatenate encoded_annotate and encoded_img) 
+        print(encoded_img.shape, encoded_annotate.shape)  
         input_x = torch.cat((encoded_img, encoded_annotate), 1)
         input_x = input_x.view(4, -1)
 
--- model/generator.py
+++ model/generator.py
@@ -28,25 +28,29 @@ class Generator(nn.Module):
         in_feature_size = self.hidden_size + self.noise_size
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d(in_feature_size, ngf * 8, 4, 1, 0, bias=False),
+            nn.ConvTranspose2d(in_feature_size, ngf * 16, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 16),
+            nn.ReLU(True),
+            # state size. (ngf*16) x 4 x 4
+            nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
-            # state size. (ngf*8) x 4 x 4
+            # state size. (ngf*8) x 8 x 8
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
-            # state size. (ngf*4) x 8 x 8
+            # state size. (ngf*4) x 16 x 16
             nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
-            # state size. (ngf*2) x 16 x 16
+            # state size. (ngf*2) x 32 x 32
             nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
-            # state size. (ngf) x 32 x 32
+            # state size. (ngf) x 64 x 64
             nn.ConvTranspose2d( ngf, 3, 4, 2, 1, bias=False),
             nn.Tanh()
-            # state size. (nc) x 64 x 64
+            # state size. (nc) x 128 x 128
         )
         
     def forward(self, noise, data):
